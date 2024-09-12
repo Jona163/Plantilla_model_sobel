13812a39649be456766e0ff743fd98b0fe3d7ef1
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-12 11:42:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,9 @@
 # Fecha: 12 Septiembre 2024
 # Descripción: Código para procesamiento de imagenes con Sobel ( plantilla).
 # GitHub: https://github.com/Jona163
+
+from flask import Flask, render_template, request, redirect, url_for
+import cv2
+import numpy as np
+import os
+from werkzeug.utils import secure_filename
