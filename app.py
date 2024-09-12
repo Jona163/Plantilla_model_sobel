06b3c22efa79d975f372f51481bf806743f8a0df
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-12 11:43:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,24 @@ import cv2
 import numpy as np
 import os
 from werkzeug.utils import secure_filename
+
+
+app = Flask(__name__)
+
+# Carpeta donde se almacenan las imágenes subidas y procesadas
+UPLOAD_FOLDER = 'static/img/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Rutas permitidas para cargar
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'txt'}
+
+@app.route('/')
+def index():
+    """ Página principal con el formulario para subir imágenes o archivos .txt """
+    return render_template('index.html')
+
+    return render_template('index.html', original_file=filename, result_image=result_image_path, file_content=file_content, result_type=result_type, error_message=error_message)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
